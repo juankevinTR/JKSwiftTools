6a7b9febdb5271e09694abd770d3ac3caaa82307
Juan Kevin Trujillo <me@juankevintrujillo.com>
2022-05-09 01:40:33 +0100

    Add logger including Emojis per levels

---
+++ Sources/JKSwiftTools/JKLogger/JKLoggerLevel.swift
@@ -0,0 +1,12 @@
+//
+//  JKLoggerLevel.swift
+//  
+//
+//  Created by Juan Kevin Trujillo on 8/5/22.
+//
+
+import Foundation
+
+private enum JKLoggerLevel: String {
+	case trace, debug, info, notice, warning, error, fault, critical
+}
